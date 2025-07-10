be9b7e457b589a7936e5223f01d7657a0748cc27
darpanhh <darpangiri5@gmail.com>
2025-07-10 21:13:41 +0545

    Final code

---
+++ binarysearch.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int BinarySearch(int *arr, int n, int data)
+{
+    int f = 0, l = n - 1;
+    int mid = (f + l) / 2;
+    while (f <= l)
+    {
+        if (data == arr[mid])
+        {
+            return mid;
+        }
+        else if (data > arr[mid])
+        {
+            f = mid + 1;
+        }
+        else
+        {
+            l = mid - 1;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 7, 8, 9, 10};
+    int n = sizeof(arr) / sizeof(int);
+    int index = BinarySearch(arr, n, 9);
+    cout<<"Element found at index :"<<index<<endl;
+    return 0;
+}
