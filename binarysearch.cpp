19134a481849994e56703386401ca72c32d61f7d
darpanhh <darpangiri5@gmail.com>
2025-07-10 21:21:37 +0545

    Corrected binary search

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 using namespace std;
+
 int BinarySearch(int *arr, int n, int data)
 {
     int f = 0, l = n - 1;
-    int mid = (f + l) / 2;
+
     while (f <= l)
     {
+        int mid = (f + l) / 2; 
+
         if (data == arr[mid])
         {
             return mid;
@@ -19,13 +22,15 @@ int BinarySearch(int *arr, int n, int data)
             l = mid - 1;
         }
     }
+
     return -1;
 }
+
 int main()
 {
     int arr[] = {1, 2, 3, 7, 8, 9, 10};
     int n = sizeof(arr) / sizeof(int);
     int index = BinarySearch(arr, n, 9);
-    cout<<"Element found at index :"<<index<<endl;
+    cout << "Element found at index: " << index << endl;
     return 0;
 }
