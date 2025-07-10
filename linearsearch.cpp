2dd359b57e4d5feeb7d561034ce5c4a2536708df
darpanhh <darpangiri5@gmail.com>
2025-07-10 20:34:04 +0545

    Initial commit

---
+++ linearsearch.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={1,2,3,4,5};
+    int n = sizeof(arr)/sizeof(int);
+    int data;
+    cout<<"Enter data which needs to be find: ";
+    cin>>data;
+    int found=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]==data){
+            cout<<"Element found at index:"<<i<<endl;
+            found = 1;
+            break;
+        }
+    }
+    if(found==0){
+        cout<<"Element not found."<<endl;
+    }
+    return 0;
+}
